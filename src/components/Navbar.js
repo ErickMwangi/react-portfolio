084050395cecacfa58117bfa24fe995aacbf23c0
ErickMwangi <rickthetri20@gmail.com>
2023-05-05 02:01:22 +0300

    Updates on navbar css

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,26 +1,32 @@
-import React from 'react'
-import '../Navbar.css'; 
+import React, { useState } from 'react';
+import '../Navbar.css';
 
 function Navbar() {
+  const [active, setActive] = useState('nav__menu');
+  const [toggleIcon, setToggleIcon] = useState('nav__toggler');
+
+  const navToggle = () => {
+    setActive(active === 'nav__menu' ? 'nav__menu nav__active' : 'nav__menu');
+    setToggleIcon(toggleIcon === 'nav__toggler' ? 'nav__toggler nav__toggler-active' : 'nav__toggler');
+  };
+
   return (
     <nav className="nav">
-      <a href='#' className='nav__brand'>ERICK</a>
-      <ul className="nav__menu">
-        <li className="nav__item"><a href='#' className="nav__link">Home</a></li>
-        <li className="nav__item"><a href='#' className="nav__link">About</a></li>
-        <li className="nav__item"><a href='#' className="nav__link">Skill</a></li>
-        <li className="nav__item"><a href='#' className="nav__link">Portfolio</a></li>
-        <li className="nav__item"><a href='#' className="nav__link">Contact</a></li>
+      <a href="#" className="nav__brand">ERICK</a>
+      <ul className={active}>
+        <li className="nav__item"><a href="#" className="nav__link">Home</a></li>
+        <li className="nav__item"><a href="#" className="nav__link">About</a></li>
+        <li className="nav__item"><a href="#" className="nav__link">Skill</a></li>
+        <li className="nav__item"><a href="#" className="nav__link">Portfolio</a></li>
+        <li className="nav__item"><a href="#" className="nav__link">Contact</a></li>
       </ul>
-      <div className= "nav__toggler">
-        <div className="line1"></div>
-        <div className="line2"></div>
-        <div className="line3"></div>
-        <div className="line4"></div>
+      <div onClick={navToggle} className={toggleIcon}>
+        <div className="line"></div>
+        <div className="line"></div>
+        <div className="line"></div>
       </div>
-
     </nav>
-  )
+  );
 }
 
 export default Navbar;
